bdff1de63fa2989c7da2ef300e6adad530f8fcc0
emmikoivisto <emetsi@gmail.com>
2020-09-26 17:26:59 +0100

    testgame set up

--- app/tests/game_test.py
+++ app/tests/game_test.py
@@ -1,6 +1,9 @@
 import unittest
 from app.models.game import Game
+from app.models.player import Player
 
 class TestGame(unittest.TestCase):
+
     def setUp(self):
-        
+        self.player1 = Player("John", "rock")
+        self.player2 = Player("Jack", "scissors")
--- app/tests/player_test.py
+++ app/tests/player_test.py
@@ -4,10 +4,12 @@ from app.models.player import Player
 class TestPlayer(unittest.TestCase):
     
     def setUp(self):
-        self.player = Player("John", "rock")
-    
-    def test_player_has_name(self):
-        self.assertEqual("John", self.player.name)
+        self.player1 = Player("John", "rock")
 
-    def test_player_has_choice(self):
-        self.assertEqual("rock", self.player.choice)
+    
+    def test_player1_has_name(self):
+        self.assertEqual("John", self.player1.name)
+
+    def test_player2_has_choice(self):
+        self.assertEqual("rock", self.player1.choice)
+
